7ee639df0d3f4f5673ee3f9420485f9e8fb68e3a
KarthikS737 <lit2021012@iiitl.ac.in>
2022-03-17 00:56:57 +0530

    Added patient graphics

--- graphic.c
+++ graphic.c
@@ -4,7 +4,7 @@ COORD c;
 #define GOTOXY(x, y) c.X = x; c.Y = y; SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 
 
-void _patientDetails(){
+void _patientDetails(int ID, char name[], int age, char number[], char email[], char gender){
     system("cls");
     MARGIN
     printf("\n\t\t\t---------------------------------------------------------\t\t\t\n");
@@ -16,15 +16,20 @@ void _patientDetails(){
     GOTOXY(40, 17)
     printf("-------------------------");
     GOTOXY(40, 20)
-    printf("Name       :  %s");
+    printf("Name       :  %s", name);
     GOTOXY(40, 22)
-    printf("Patient ID :  %d");
+    printf("Patient ID :  %d", ID);
     GOTOXY(40, 24)
-    printf("Gender     :  %c");
+    printf("Age        :  %d", age);
     GOTOXY(40, 26)
-    printf("Contact    :  %s");
+    printf("Gender     :  %c", gender);
     GOTOXY(40, 28)
-    printf("Email      :  %s");
+    char contactNum[11];
+    for(int i = 0; i < 10; ++i)
+        contactNum[i] = number[i];
+    printf("Contact    :  %s", contactNum);
+    GOTOXY(40, 30)
+    printf("Email      :  %s", email);
     
     for(int i = 13; i < 42; ++i){
         GOTOXY(24, i); printf("|\n");
@@ -36,4 +41,5 @@ void _patientDetails(){
 
 
     printf("\t\t\t---------------------------------------------------------\t\t\t\n");
+   
 }
--- patient.c
+++ patient.c
@@ -6,7 +6,7 @@ typedef struct patient{
     char lastName[20];
     int age;
     char gender;
-    char contactNumber[10];
+    char contactNumber[11];
     char email[30];
     char passwd[20];
     } patient;
@@ -14,14 +14,14 @@ typedef struct patient{
 
 // ********************************************** PATIENT FILE INSERTING **********************************************
 // Insert data into patientLog.csv
-void _patientInsertCreds(ull ID, char name[], int age, char contact[], char email[]){
-    FILE * patientFile = fopen("./files/patientLog.csv", "w");
+void _patientInsertCreds(ull ID, char name[], int age, char contact[], char email[], char gender){
+    FILE * patientFile = fopen("./files/patientLog.csv", "a");
     if(patientFile == NULL){
         printf("\t\t\t Service currently unavailable !!\n");
         printf("\t\t\t Please press and key to return.....\n");
         getch();
     } else {
-        fprintf(patientFile, "%llu, %s, %d, %s, %s\n", ID, name, age, contact, email );
+        fprintf(patientFile, "%llu, %s, %d, %s, %s, %c\n", ID, name, age, contact, email, gender );
         xsmMARGIN
         printf("\t\t\tPatient added successfully");
     }
@@ -31,7 +31,7 @@ void _patientInsertCreds(ull ID, char name[], int age, char contact[], char emai
 
 // Store patient ID and password 
 void _patientStorePasswd(ull ID, char passwd[]){
-    FILE * passwdFile = fopen("./files/patientPasswd.csv", "w");
+    FILE * passwdFile = fopen("./files/patientPasswd.csv", "a");
     if(passwdFile == NULL){
         printf("\t\t\t Error saving passwrd retry again !!\n");
         printf("\t\t\t Please press and key to return.....\n");
@@ -167,7 +167,7 @@ void patientReg(){
     } while (!validate);
 
     if(validate){
-        _patientInsertCreds(p.ID, name, p.age, p.contactNumber, p.email);
+        _patientInsertCreds(p.ID, name, p.age, p.contactNumber, p.email, p.gender);
         _patientStorePasswd(p.ID, p.passwd);
         printf("\n\t\t\t\t Your Patient ID is %d", p.ID);
         MARGIN
@@ -206,6 +206,49 @@ char * _authorizeLogin(int ID){
     return errorCode;
 }
 
+patient __CurrentPatient;
+char __currentPatientName[100];
+void _fetchPatientDetails(int ID){
+    FILE * patientFile = fopen("./files/patientLog.csv", "r");
+    if(!patientFile){
+        printf("\t\t\t Services Currently unavailable Sorry for the inconvinience!... !!\n");
+        printf("\t\t\t Please press and key to return.....\n");
+        getch();
+    } else {
+        char line[200];
+        int row = 0;
+        int column = 0;
+
+        while (fgets(line, sizeof(line), patientFile)) {
+            char *value;
+            value = strtok(line, ",");
+            if(atoi(value) == ID){
+                __CurrentPatient.ID = ID;
+                value = strtok(NULL, ",");
+                strcpy(__currentPatientName, value);
+                value = strtok(NULL, ",");
+                __CurrentPatient.age = atoi(value);
+                value = strtok(NULL, ",");
+                char number[10];
+                strcpy(number, value);
+                strcpy(__CurrentPatient.contactNumber, number);
+
+                printf("%s num", __CurrentPatient.contactNumber);
+                value = strtok(NULL, ",");
+                strcpy(__CurrentPatient.email, value);
+                value = strtok(NULL, ",");
+                if (strcmp(value, "M"))
+                    __CurrentPatient.gender = 'M';
+                else if (strcmp(value, "F"))
+                    __CurrentPatient.gender = 'F';
+                else 
+                    __CurrentPatient.gender = 'O';
+            }
+        }
+        
+    }
+}
+
 
 
 
@@ -220,8 +263,10 @@ void patientLog(){
     printf("\t\t\t Enter the patient ID : ");
     gets(Identify);
     int ID  = atoi(Identify);
-    char * a = _authorizeLogin(ID); // returns the password as a string pointer
-    if(!strcmp(a, "error__")){
+    char * passwd = _authorizeLogin(ID); // returns the password as a string pointer
+    char pass[100];
+    strcpy(pass, passwd);
+    if(!strcmp(passwd, "error__")){
         xsmMARGIN
         printf("\t\t\t ID not found.....\n");
         printf("\t\t\t Please press and key to return.....\n");
@@ -229,8 +274,8 @@ void patientLog(){
         getch();
         return;
     } else{
-        _patientDetails();
-        
+        _fetchPatientDetails(ID);
+        _patientDetails(__CurrentPatient.ID, __currentPatientName, __CurrentPatient.age, __CurrentPatient.contactNumber, __CurrentPatient.email, __CurrentPatient.gender); 
     }
     MARGIN
 }
